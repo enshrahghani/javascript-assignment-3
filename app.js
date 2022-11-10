069d3a15067f8768b237cc257d1ee5775f87c16a
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-11-10 13:00:02 +0500

    javascript assignment 3

---
+++ app.js
@@ -0,0 +1,74 @@
+var animal0 = "Gorilla"
+var animal1 = "Wolf"
+var animal2 = "Tiger"
+var animal3 = "Fox"
+var animal4 = "Lion"
+var animal5 = "Deer"
+
+alert("the most powerful animal " + animal4)
+
+
+var flowers = ["Rose" , "Jasmine" , "tulip" , "Daffodil" , "Lotus" , "Aster"]
+
+console.log(flowers)
+console.log(flowers [2])
+
+
+var alpha = ["a" ,"b" , "c" , "d" , "e" , "f"]
+
+alpha. push("g")
+console.log(alpha)
+
+
+
+var fruits = ["apple" , "kiwi" , "mango" , "grapes" , "watermelon"]
+
+fruits. pop()
+console.log(fruits)
+
+
+
+var numbers = ["0" , "1" , "2" , "3" , "4" , "5" , "6" , "7"]
+
+numbers. shift()
+console.log(numbers)
+
+
+
+var birds = ["crow" , "pigeon" , "owl" , "eagle" , "parrot"]
+
+birds. unshift("sparrow")
+console.log(birds)
+
+
+
+var names = ["hira" , "amna" , "kinza" , "zehra" , "laiba" , "ume" , "erum"]
+
+var z = names. slice(3,6)
+console.log(z)
+
+
+
+var guest = ["ali" ,"abdul" , "fahad" , "hamza" , "farhan"]
+
+guest. splice(1,3)
+console.log(guest)
+
+
+// var h = ["j" , "k" , "l" , "m" , "n" , "o" , "p"]
+
+// var h = h.indexof ("n")
+// console.log(h)
+
+
+
+// var alphabtes = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k"]
+
+// var z = net.length
+// console.log(z)
+
+
+
+for (var k = 0; k < 10; k++) {
+    console.log("2 X " + (k + 1) + " = " + 2 * (k + 1))
+}
